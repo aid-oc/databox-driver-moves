f4b1c24fdf9ecf8429cd8ebf38b4a954db8265a6
Aidan O'Connell <psyaoc@gmail.com>
2017-11-11 15:04:33 +0000

    working (out of databox)

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//var databox = require('node-databox');
+var fs = require('fs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// JSON Store
+//var DATABOX_STORE_BLOB_ENDPOINT = process.env.DATABOX_STORE_ENDPOINT;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,10 +42,29 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+/*
+function getPastDayData() {
+
+}
+
+// Wait until store is ready and then initalise it
+databox.waitForStoreStatus(DATABOX_STORE_BLOB_ENDPOINT, 'active', 100).then(() => {
+  databox.catalog.registerDatasource(
+    DATABOX_STORE_BLOB_ENDPOINT, {
+              description: 'Moves API Storage',
+              contentType: 'text/json',
+              vendor: 'Databox Inc.',
+              type: 'movesApiStorage',
+              datasourceid: 'MovesApiStorage',
+              storeType: 'databox-store-blob',
+            });
+});
+*/
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var app = require('../app.js');
+var movesApi = require('moves-api').MovesApi;
+var moves = new movesApi({
+    "clientId": "9ko4K08GS9BKuFVej6Zw9SZTiVWvscHp",
+    "clientSecret": "qeIdV7DIi0Uk0OLeF2aDT4XIU_zZSxBbQ3PltGiE4cPEHo6i0M6eP4j9RGrj6__h",
+    "redirectUri": "http://localhost:3000/token",
+    "accessToken": "",
+    "refreshToken" : "",
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (moves.options.accessToken == "") {
+    // Redirect to start auth process
+    var url = moves.generateAuthUrl();
+    res.redirect(url);
+  } else {
+    var placesOptions = {
+      "date": "20171111"
+    }
+    var placesSummary = moves.getPlaces(placesOptions, function(err, body) {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(body);
+      }
+    });
+  }
+
 });
+
+router.get('/token', function(req, res, next) {
+  // Return to here with an access code, exchange for token
+  moves.getAccessToken(req.query.code, function(err, authData) {
+      moves.options.accessToken = authData.access_token;
+      moves.getProfile(function(err, profile) {
+          console.log("Connected to Profile: " + profile);
+          res.redirect("/");
+      });
+    });
+});
+
 
 module.exports = router;
