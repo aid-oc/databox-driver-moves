7defe3807b7db58a40c6f707ed6ae2bf0f966419
Aidan O'Connell <psyaoc@gmail.com>
2017-11-24 05:50:27 +0000

    function to store profile

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ var moves = new movesApi({
 
 var AUTH_REDIRECT_URL = "/#!/databox-driver-moves/ui";
 
+/** Checks to see if we have an access token stored, this will then be verified and refreshed if necessary 
+(saves re-inputting client details each time */
 function verifyAccessToken() {
   databox.keyValue.read(storeHref, 'movesToken').then((res) => {
     console.log("Token found: " + res.access_token);
@@ -50,6 +52,7 @@ function verifyAccessToken() {
   });
 }
 
+/** Stores the current Client ID/Client Secret for the Moves Application */
 function storeAppCredentials(clientId, clientSecret) {
   var movesCredentials = {id: clientId, secret: clientSecret};
   databox.keyValue.write(storeHref, 'movesCredentials', movesCredentials).then((res) => {
@@ -60,6 +63,7 @@ function storeAppCredentials(clientId, clientSecret) {
   });
 }
 
+/** Gets the current Client ID/Client Secret for the Moves Application */
 function getAppCredentials() {
   databox.keyValue.read(storeHref, 'movesCredentials').then((res) => {
     console.log("Credentials found: " + res);
@@ -70,7 +74,30 @@ function getAppCredentials() {
   });
 }
 
-// Entry point, form for credentials input
+/** Stores some basic information about the moves user, client ID/platform */
+function storeMovesProfile() {
+  var userProfile = moves.getProfile(function(err, profile) {
+    if (err) {
+      console.log("Error: Unable to retrieve profile");
+    } else {
+      var userId = profile.userId;
+      var userPlatform = profile.platform;
+      databox.keyValue.write(storeHref, 'movesUserId', userId).then((res) => {
+        console.log("Stored Crendentials");
+      }).catch(() => {
+        console.log("Failed to store credentials");
+      });
+      databox.keyValue.write(storeHref, 'movesUserPlatform', userPlatform).then((res) => {
+        console.log("Stored Platform");
+      }).catch(() => {
+        console.log("Failed to store platform");
+      });
+    }
+  });
+}
+
+
+/** Driver home, will display data with a valid access token or begin authentication if necessary */
 router.get('/', function(req, res, next) {
   // Just check if we have a stored access token which can be refreshed
   verifyAccessToken();
@@ -92,7 +119,7 @@ router.get('/', function(req, res, next) {
   }
 });
 
-/* Auth route, will create an auth code and redirect to /authtoken, where a token is created and stored */
+/** Auth route, will create an auth code and redirect to /authtoken, where a token is created and stored */
 router.post('/auth', function(req, res, next) {
   if (moves.options.accessToken == "") {
     moves.options.clientId = req.body.clientId;
@@ -108,9 +135,8 @@ router.post('/auth', function(req, res, next) {
   }
 });
 
-/* Create a token from the current auth code, store it and redirect to show data */
+/** Request an access token using a valid authenticate code, store it and redirect back to home */
 router.get('/authtoken', function(req, res, next) {
-  // Return to here with an access code, exchange for token
   moves.getAccessToken(req.query.code, function(err, authData) {
     if (err) {
       console.log(err);
